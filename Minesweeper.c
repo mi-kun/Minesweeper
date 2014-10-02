1fdec3f32ff0b01d4856971a0ce364b59b946396
mi-kun <mi-kun@npca-1.info>
2014-10-02 17:54:40 +0900

    update Minesweeper.c

--- Minesweeper.c
+++ Minesweeper.c
@@ -5,11 +5,11 @@ int getrandom(int min,int max);
 
 int main(void)
 {
-	int bord[10][10],bord2[10][10],i,j,k,level,a,b,fin=100;//bord:そのマスがどのようなマスかの情報　bord2:そのマスが公開されてるかどうかの情報　fin:終了条件
+	int bord[12][12],bord2[12][12],i,j,k,level,a,b,fin=100;//bord:そのマスがどのようなマスかの情報　bord2:そのマスが公開されてるかどうかの情報　fin:終了条件
 
-	for(i=0;i<10;i++)
+	for(i=1;i<11;i++)
 	{
-		for(j=0;j<10;j++)
+		for(j=1;j<11;j++)
 		{
 			bord[i][j]=0;
 			bord2[i][j]=0;
@@ -21,31 +21,44 @@ int main(void)
 		fin=100;
 		printf("レベルを入力してください");
 		scanf("%d",&level);
-		
+
 		fin-=level*4;
 
 		for(i=1;i<=level*4;) //地雷の位置
 		{
-			a=getrondom(0,9);
-			b=getrondom(0,9);
-
+			a=getrondom(1,10);
+			b=getrondom(1,10);
+			
 			if(bord[a][b]==0)
 			{
 				i++;
 				bord[a][b]=-1;
-				for(j=)
+				for(j=a-1;j<=a+1;j++)
+				{
+					for(k=b-1;k<=b+1;k++)
+					{
+						if(bord[j][k]!=-1)
+						{
+							bord[j][k]++;
+						}
+					}
+				}
 			}
 		}
-
+	
 		scanf("%d %d",&i,&j); //どのマスを選んだか
 		if(bord[i][j]==-1)
 		{
 			printf("gameover\n");
 			continue;
 		}
+		if(bord[i][j]>0)
+		{
+			bord2[i][j]=1;
+		}
 		if(bord[i][j]==0)
 		{
-			
+			bord
 		}
 	}
 }
