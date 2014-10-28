e0c3f5f5b18376343c53d1eca4f6c23c79d90413
mi-kun <mi-kun@npca-1.info>
2014-10-28 22:38:00 +0900

    Update Minesweeper.c

--- Minesweeper.c
+++ Minesweeper.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 enum BLOCK_TYPE {MINE=-1,SAFE=0};
 #define FIELD_SIZE 12
 
-int bord[FIELD_SIZE][FIELD_SIZE];
-int bord2[FIELD_SIZE][FIELD_SIZE];
+int board[FIELD_SIZE][FIELD_SIZE];
+int board2[FIELD_SIZE][FIELD_SIZE];
 int fin;
+int h[] = { 0 , 1 , 0 , -1 ,  0};
+int w[] = { 0 , 0 , 1 ,  0 , -1};
 
 int getrandom(int min , int max);
 void space(int height , int width);
@@ -14,73 +17,136 @@ void output(void);
 
 int main(void)
 {
-	int i;
+	int i=0;
 	int j;
 	int k;
 	int a , b;
 	int level;
+	int x , y;
+	int n , m;
+
+	srand((unsigned int)time(NULL));
 
 	for(i = 1; i < FIELD_SIZE - 1; i++)
 	{
 		for(j = 1; j < FIELD_SIZE - 1; j++)
 		{
-			bord[i][j] = SAFE;
-			bord2[i][j] = SAFE;
+			board[i][j] = SAFE;
+			board2[i][j] = 0;
 		}
 	}
 
-	fin = 100;
-	printf("レベルを入力してください");
-	scanf("%d", &level);
+	fin = (FIELD_SIZE - 2) * (FIELD_SIZE - 2);
+	printf("Is level (1-6): ");
+	for(i = 1 ; i <= 1 ; i++)
+	{
+		scanf("%d" , &level);
+		if(level < 1 || level > 6)
+		{
+			printf("Please continue.\n");
+			continue ;
+		}
+	}
+	
+	printf("Is coordinate (1-10) (1-10) : ");
+	for(i = 1 ; i <= 1 ; i++)
+	{
+		scanf("%d" , &x);
+		scanf("%d" , &y);
 
-	fin -= level * 4;
+		if(x < 1 || x > 10 || y < 1 || y > 10)
+		{
+			printf("Please continue.\n");
+			continue ;
+		}
+	}
+	
+	fin -= level * 10;
 
-	while(i <= level * 4)
+	i=0;
+
+	while(i <= level * 10)
 	{
 		a = getrandom(1 , 10);
+		
 		b = getrandom(1 , 10);
 
-		if(bord[a][b] == SAFE)
+		if(board[a][b] != MINE)
 		{
+			for(n = x-1 ; n <= x+1 ; n++)
+			{
+				for(m = y-1 ; m <= y+1 ; m++)
+				{
+					if(a == n && b == m)
+					{
+						break;
+					}
+				}
+				if(a == n && b == m)
+				{
+					break;
+				}
+			}
+			
+			if(a == n && b == m)
+			{
+				continue;
+			}
+			
 			i++;
-			bord[a][b] = MINE;
+			board[a][b] = MINE;
 			for(j = a - 1; j <= a + 1; j++)
 			{
 				for(k = b - 1; k <= b + 1; k++)
 				{
-					if(bord[j][k] != -1)
+					if(board[j][k] != MINE)
 					{
-						bord[j][k]++;
+						board[j][k]++;
 					}
 				}
 			}
 		}
 	}
 	
+	space(x , y);
+
+	output();
+
 	while(1)
 	{
-		scanf("%d %d", &i , &j);
+		printf("Is coordinate (1-10) (1-10) : ");
+		scanf("%d" , &i);
+		scanf("%d" , &j);
 
-		if(bord[i][j] == MINE)
+		if(board2[i][j]==1 || i < 1 || i > FIELD_SIZE-2 || j < 1 || i > FIELD_SIZE-2)
 		{
-			printf("gameover\n");
-			break;
+			printf("Please continue\n");
+			continue;
 		}
-
-		if(bord[i][j] > SAFE)
+		else
 		{
-			bord2[i][j] = 1;
-			fin--;
-		}
+			if(board[i][j] == MINE)
+			{
+				printf("gameover\n");
+				break;
+			}
 
-		if(bord[i][j] == SAFE)
-		{
-			space(i,j);
-		}
+			if(board[i][j] > SAFE)
+			{
+				board2[i][j] = 1;
+				fin--;
+			}
 
-		if(fin == 0)
-		{
-			printf("クリア\n");
+			if(board[i][j] == SAFE)
+			{
+				space(i,j);
+			}
+
+			if(fin == 0)
+			{
+				printf("crear\n");
+				break;
+			}
 		}
 
 		output();
@@ -95,18 +161,25 @@ int getrandom(int min , int max)
 void space(int height , int width)
 {
 	int i;
-	int h[] = { 1 ,  1 ,  0 , -1 , -1 , -1 ,  0 ,  1};
-	int w[] = { 0 ,  1 ,  1 ,  1 ,  0 , -1 , -1 , -1};
 
-	for (i = 0; i < 8; ++i)
+	for (i = 0; i < 4; ++i)
 	{
-		if ((height + h[i]) < 11 && (height + h[i]) > 0 && (width + w[i]) < 11 && (width + w[i]) > 0)
+		if ((height + h[i]) < FIELD_SIZE-1 && (height + h[i]) > 0 && (width + w[i]) < FIELD_SIZE-1 && (width + w[i]) > 0)
 		{
-			if (bord2[height + h[i]][width + w[i]] == 0 && bord[height+h[i]][width+w[i]] != -1)
+			if (board2[height + h[i]][width + w[i]] == 0)
 			{
-				bord2[height + h[i]][width + w[i]] = 1;
-				fin--;
-				space(height + h[i] , width + w[i]);
+				if(board[height + h[i]][width + w[i]]==SAFE)
+				{
+					board2[height + h[i]][width + w[i]]=1;
+					fin--;
+					space(height + h[i] , width + w[i]);
+				}
+				else if(board[height + h[i]][width + w[i]] != MINE)
+				{
+					board2[height + h[i]][width + w[i]] = 1;
+					fin--;
+					return ;
+				}
 			}
 		}
 	}
@@ -116,26 +189,21 @@ void space(int height , int width)
 
 void output(void)
 {
+	printf("output\n");
+
 	int i;
 	int j;
 	for(i=1;i<11;i++)
 	{
 		for(j=1;j<11;j++)
 		{
-			if(bord2[i][j]==0)
+			if(board2[i][j]==1)
 			{
-				if(bord[i][j]==SAFE)
-				{
-					printf("   ");
-				}
-				else
-				{
-					printf(" %d ",bord[i][j]);
-				}
+				printf(" %d ",board[i][j]);
 			}
 			else
 			{
-				printf("   ");
+				printf(" = ");
 			}
 		}
 		puts("");
